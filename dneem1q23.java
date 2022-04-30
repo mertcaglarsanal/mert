6d099babc7149424e151c0c295e7e888e469bb9e
teqh <104692610+teqh@users.noreply.github.com>
2022-04-30 21:15:04 +0300

    Add files via upload

---
+++ dneem1q23.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class dneem1q23 {
+    public static void main(String[]args){
+        Scanner t = new Scanner(System.in);
+       int basamak = t.nextInt();//kullanıcıdan basamağı aldım
+        int toplam = 0,sayı =1;
+
+        for(int x =0,onbasamak=1;x < basamak;x++) {
+            onbasamak *= 10;
+            toplam += sayı;
+        sayı += onbasamak;
+            System.out.println(sayı);
+            System.out.println(toplam);
+        }
+    }
+}
